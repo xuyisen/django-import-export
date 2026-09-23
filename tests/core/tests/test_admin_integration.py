52c0102e96e5e4206a7a98d5c4ecae3fb6a5d374
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:40:59 -0400

    xuyisen__diff__id_29

--- tests/core/tests/test_admin_integration.py
+++ tests/core/tests/test_admin_integration.py
@@ -680,7 +680,9 @@ class ExportAdminIntegrationTest(AdminTestMixin, TestCase):
             "file_format": "0",
         }
         date_str = datetime.now().strftime("%Y-%m-%d")
-        with self.assertNumQueries(7):  # Should not contain COUNT queries from ModelAdmin.get_results()
+        with self.assertNumQueries(
+            7
+        ):  # Should not contain COUNT queries from ModelAdmin.get_results()
             response = self.client.post("/admin/core/book/export/", data)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.has_header("Content-Disposition"))
