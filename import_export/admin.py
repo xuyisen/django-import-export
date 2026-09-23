8ecb6d65fbbb30cb9419277eabc3cdc4719c8f1e
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:41:05 -0400

    xuyisen__diff__id_31

--- import_export/admin.py
+++ import_export/admin.py
@@ -746,14 +746,16 @@ class ExportMixin(BaseExportMixin, ImportExportMixinBase):
             def get_results(self, request):
                 """
                 We override this method because we only call ChangeList.get_queryset()
-                so we don't need anything from this method. The get_results() gets called during
+                we don't need anything else from this method. The get_results()
+                gets called during
                 ChangeList.__init__() and we do want to avoid unnecessary COUNT queries.
                 """
                 pass
 
         cl = ExportChangeList(**changelist_kwargs)
 
-        # get_queryset() is already called during initialization, it is enough to get it's results
+        # get_queryset() is already called during initialization, it is enough
+        # to get its results
         if hasattr(cl, "queryset"):
             return cl.queryset
 
